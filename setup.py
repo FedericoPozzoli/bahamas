1b60f715c42af693f4553df89c4e64f160c51368
Riccardo Buscicchio <riccardo.buscicchio@unimib.it>
2025-08-23 03:33:55 +0200

    Bump version from 0.1.0 to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bahamas',
-    version='0.1.0',
+    version='0.1.1',
     description='BAyesian HAmiltonian Montecarlo Analysis for Stochastic gravitational wave signal',
     author='Federico Pozzoli',
     author_email='fpozzoli@uninsubria.it',
